afd41aaf5745ac83f7ee2c747dc2448c4fdf4422
bkisly <b.kisly@gmail.com>
2022-07-07 02:43:29 +0200

    Implemented basic player movement

--- Dash Ninja/Assets/Scripts/PlayerMovement.cs
+++ Dash Ninja/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveTime = .1f;
+
+    private float _currentMoveTime = 0f;
+    private Vector3 _newPosition = new();
+    private bool _isMoving = false;
+
     private void Start()
     {
         InputManager.Instance.KeyPressed += InputManager_KeyPressed;
     }
 
+    private void Update()
+    {
+        if(_isMoving)
+        {
+            _currentMoveTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, _newPosition, _currentMoveTime / moveTime);
+
+            if(_currentMoveTime >= moveTime)
+            {
+                _currentMoveTime = 0f;
+                _isMoving = false;
+            }
+        }
+    }
+
     private void InputManager_KeyPressed(Direction direction)
     {
-        Debug.Log(direction.ToString());
+        if (_currentMoveTime == 0f)
+        {
+            switch (direction)
+            {
+                case Direction.Up:
+                    _newPosition = transform.position + Vector3.forward;
+                    break;
+                case Direction.Down:
+                    _newPosition = transform.position + Vector3.back;
+                    break;
+                case Direction.Left:
+                    _newPosition = transform.position + Vector3.left;
+                    break;
+                case Direction.Right:
+                    _newPosition = transform.position + Vector3.right;
+                    break;
+            }
+
+            _isMoving = true;
+        }
     }
 }
